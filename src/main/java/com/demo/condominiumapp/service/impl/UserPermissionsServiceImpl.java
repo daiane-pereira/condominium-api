f61b0981afc626f843a935582524b96767fe4d44
Daiane Pereira Kuwiecinski <daiane.sper@gmail.com>
2021-03-11 11:34:22 -0300

    :construction: Implementing filter by condominium.

--- src/main/java/com/demo/condominiumapp/service/impl/UserPermissionsServiceImpl.java
+++ src/main/java/com/demo/condominiumapp/service/impl/UserPermissionsServiceImpl.java
@@ -1,6 +1,9 @@
 package com.demo.condominiumapp.service.impl;
 
+import com.demo.condominiumapp.domain.FunctionalityPermission;
 import com.demo.condominiumapp.domain.Group;
+import com.demo.condominiumapp.domain.UserPermission;
+import com.demo.condominiumapp.enumerator.PermissionEnum;
 import com.demo.condominiumapp.factory.OutputFactory;
 import com.demo.condominiumapp.service.UserPermissionsService;
 import org.springframework.stereotype.Service;
@@ -13,12 +16,78 @@ public class UserPermissionsServiceImpl implements UserPermissionsService {
 
     @Override
     public List<String> formatUserPermissions(List<Group> userGroups) {
+        List<UserPermission> userPermissions = new ArrayList<>();
         List<String> userPermissionsOutput = new ArrayList<>();
+
         userGroups.forEach(group -> {
-            userPermissionsOutput.add(OutputFactory.buildFrom(
-                    group.getCondominiumId(),
-                    group.getFunctionalityPermissionList()));
+            Boolean containsCondominium = containsCondominiumInList(userPermissions, group.getCondominiumId());
+            if (containsCondominium) {
+                UserPermission userPermissionAuxiliary = userPermissions.stream()
+                        .filter(up -> up.getCondominiumId() == group.getCondominiumId())
+                        .findFirst()
+                        .get();
+
+                List<FunctionalityPermission> functionalityPermissionsNew = new ArrayList<>();
+
+                userPermissionAuxiliary.getFunctionalityPermissionList().forEach(functionalityPermission -> {
+                    String functionalityAuxiliary = functionalityPermission.getFunctionality();
+                    List<String> permissions = new ArrayList<>();
+                    permissions.add(functionalityPermission.getPermission());
+
+                    FunctionalityPermission fp = group.getFunctionalityPermissionList().stream()
+                            .filter(f -> f.getFunctionality().equals(functionalityAuxiliary))
+                            .findFirst()
+                            .get();
+
+                    permissions.add(fp.getPermission());
+                    String relevancePermission = getPermissionWithMoreRelevance(permissions);
+                    functionalityPermissionsNew.add(FunctionalityPermission.builder()
+                            .functionality(functionalityAuxiliary)
+                            .permission(relevancePermission)
+                            .build());
+                });
+
+                userPermissions.add(UserPermission.builder()
+                        .condominiumId(group.getCondominiumId())
+                        .functionalityPermissionList(functionalityPermissionsNew)
+                        .build());
+
+            } else {
+                userPermissions.add(UserPermission.builder()
+                        .condominiumId(group.getCondominiumId())
+                        .functionalityPermissionList(group.getFunctionalityPermissionList())
+                        .build());
+            }
         });
+
+        userPermissions.forEach(userPermission -> {
+            userPermissionsOutput.add(OutputFactory.buildFrom(
+                    userPermission.getCondominiumId(),
+                    userPermission.getFunctionalityPermissionList()));
+        });
+
         return userPermissionsOutput;
+    }
+
+    private Boolean containsCondominiumInList(List<UserPermission> userPermissions, Long condominiumId) {
+        for (UserPermission permission: userPermissions) {
+            if (permission.getCondominiumId() == condominiumId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getPermissionWithMoreRelevance(List<String> permissions) {
+        for (String permission: permissions) {
+            if (permission.equals(PermissionEnum.WRITING.getValue())) {
+                return permission;
+            }
+
+            if (permission.equals(PermissionEnum.READING.getValue())) {
+                return permission;
+            }
+        }
+        return PermissionEnum.NONE.getValue();
     }
 }
